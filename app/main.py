d7fd2f235b4572efae9e037f537598248104807b
Alex <alexx5039@gmail.com>
2025-11-08 15:56:43 +0200

    copy file implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if not command.strip():
+        return
+    command_file = command.split()
+    if len(command_file) != 3 or command_file[0] != "cp":
+        return
+    if command_file[1] == command_file[2]:
+        return
+    try:
+        with (
+            open(command_file[1], "r") as file_in,
+            open(command_file[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
