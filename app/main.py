30bf0f2298e9da968203b4263ca97de689a0ea08
Alex <alexx5039@gmail.com>
2025-11-09 08:42:12 +0200

    changes implemented

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def copy_file(command: str) -> None:
     if not command.strip():
         return
-    command_file = command.split()
-    if len(command_file) != 3 or command_file[0] != "cp":
+    command_parts = command.split()
+    if len(command_parts) != 3 or command_parts[0] != "cp":
         return
-    if command_file[1] == command_file[2]:
+    _, source_file, destination_file = command_parts
+    if source_file == destination_file:
         return
     try:
         with (
-            open(command_file[1], "r") as file_in,
-            open(command_file[2], "w") as file_out
+            open(source_file, "r") as file_in,
+            open(destination_file, "w") as file_out
         ):
             file_out.write(file_in.read())
     except FileNotFoundError:
